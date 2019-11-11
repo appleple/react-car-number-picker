709daf7ba4eb80d8247a57a47592342369001531
Hori Godai <ess_president@me.com>
2019-11-11 14:39:34 +0900

    chore(build)

--- lib/src/number-picker.d.ts
+++ lib/src/number-picker.d.ts
@@ -1,10 +1,12 @@
 /// <reference types="react" />
+declare type Key = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '.' | '*';
 declare type Props = {
     value: string;
     onChange(value: string): void;
     onOpen(): void;
     onClose?(): void;
     open?: boolean;
+    disableds: Key[];
 };
 declare const _default: (props: Props) => JSX.Element;
 export default _default;
